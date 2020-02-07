eb221de1900e7f88e0d102327f4c1fb9f65ffc37
linyang <332319223@qq.com>
2020-02-07 11:03:36 +0800

    code refactor

--- download_res_from_ai_plat.py
+++ download_res_from_ai_plat.py
@@ -13,131 +13,13 @@ from ustr import ustr
 
 import json
 import os
+import logging
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=logging.DEBUG)
+
 
 XML_EXT = '.xml'
-# ENCODE_METHOD = DEFAULT_ENCODING
-
-
-def threadWork(resource_id, version_id, headers, path):
-    img_dir = path + '/' + 'img'
-    if not os.path.isdir(img_dir):
-        os.makedirs(img_dir)
-    url_download_res_label = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/download_res_label/?label_version_id=' + str(
-        version_id) + '&' + 'res_id=' + str(resource_id)
-    response = requests.get(url_download_res_label, headers=headers)
-    res_data_and_label = json.loads(response.text)
-    res_data = res_data_and_label['res_data']
-    res_name = res_data_and_label['res_name']
-    res_label = res_data_and_label['res_label']
-    label = json.loads(res_label)
-    if len(label) == 1:
-        res_data = res_data[0]
-        img = base64.b64decode(res_data)
-        file = open(img_dir + '/' + res_name, 'wb')
-        file.write(img)
-        file.close()
-    else:  # res_name 需要改变一下
-        counter = 0
-        for sub_img in res_data:
-            img = base64.b64decode(sub_img)
-            res_name_list = res_name.split('.')
-            file = open(img_dir + '/' + res_name_list[0] + '_' + str(counter) + '.jpg', 'wb')
-            counter += 1
-            file.write(img)
-            file.close()
-
-    # get xml
-    xml_dir = path + '/' + 'xml'
-    if '.jpg' in res_name:
-        res_name = res_name.split('.jpg')[0]
-    elif '.png' in res_name:
-        res_name = res_name.split('.png')[0]
-    else:
-        res_name = res_name.split('.')[0]
-    if not os.path.isdir(xml_dir):
-        os.makedirs(xml_dir)
-
-    counter = 0
-    for name in label:
-        if name == '':
-            vocwriter = PascalVocWriter(foldername=xml_dir, filename=res_name, imgSize=(
-                0, 0), databaseSrc='Unknown', localImgPath=xml_dir)
-            for i in label[name]:
-                minx = int(i['vertexs'][0][0])
-                miny = int(i['vertexs'][0][1])
-                maxx = int(i['vertexs'][1][0])
-                maxy = int(i['vertexs'][1][1])
-                name = i['name']
-                vocwriter.addBndBox(minx, miny, maxx, maxy, name, False)
-            xml_name = res_name + ".xml"
-            xml_path = os.path.join(xml_dir, xml_name)
-            vocwriter.save(targetFile=xml_path)
-        else:
-            vocwriter = PascalVocWriter(foldername=xml_dir, filename=res_name, imgSize=(
-                0, 0), databaseSrc='Unknown', localImgPath=xml_dir)
-            for i in label[name]:
-                minx = int(i['vertexs'][0][0])
-                miny = int(i['vertexs'][0][1])
-                maxx = int(i['vertexs'][1][0])
-                maxy = int(i['vertexs'][1][1])
-                name = i['name']
-                vocwriter.addBndBox(minx, miny, maxx, maxy, name, False)
-            xml_name = res_name + '_' + str(counter) + ".xml"
-            counter += 1
-            xml_path = os.path.join(xml_dir, xml_name)
-            vocwriter.save(targetFile=xml_path)
-
-
-def login(email, password, url):  # 登陆模块
-    # url = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/auth/'
-    data = json.dumps({'email': email, 'password': password})
-    response = requests.post(url, data)  # 发出请求
-    res = json.loads(response.text)
-    if res != -1 and res != 0:
-        token = res['token']
-    else:
-        return {'error': '账号或密码错误'}
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'jwt' + ' ' + token
-    }
-    return headers
-
-def download_train_res(project, version, path, headers):  #项目 版本 本地保存路径
-    url_prj = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/projects'
-    response = requests.get(url_prj, headers=headers)
-    prj_list = json.loads(response.text)
-    for i in prj_list:
-        if project==i['name']:
-            project_id = i['id']
-    url_version = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/label_versions/?pid=' + str(project_id)
-    response = requests.get(url_version, headers=headers)
-    version_list = json.loads(response.text)
-    for i in version_list:
-        if version==i['name']:
-            version_id = i['id']
-    url_get_resource_id_list = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/get_res_id_list/?label_version_id=' + str(
-        version_id)
-    response = requests.get(url_get_resource_id_list, headers=headers)
-    resource_id_list = json.loads(response.text)
-    for resource_id in resource_id_list:
-        t = Thread(target=threadWork, args=(resource_id, version_id, headers, path))
-        t.start()
-
-
-def testDownloadTrainRes():
-    email = 'sys_admin'
-    password = 'hongpu'
-    url = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/auth/'
-    headers = login(email, password, url)
-    project = '孙可标签测试'
-    version = 'v1'
-    path = r'C:\Users\86157\Desktop\testlin'
-    download_train_res(project, version, path, headers)
-
-testDownloadTrainRes()
-
-
+ENCODE_METHOD = 'utf-8'
 
 
 class PascalVocWriter:
@@ -250,4 +132,143 @@ class PascalVocWriter:
         prettifyResult = self.prettify(root)
         out_file.write(prettifyResult.decode('utf8'))
         out_file.close()
+
+
+def threadWork(resource_id_list, version_id, headers, path):
+    imgNumber = len(resource_id_list)
+    index = 0
+    for resource_id in resource_id_list:
+        img_dir = path + '/' + 'img'
+        if not os.path.isdir(img_dir):
+            os.makedirs(img_dir)
+        url_download_res_label = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/download_res_label/?label_version_id=' + str(
+            version_id) + '&' + 'res_id=' + str(resource_id)
+        response = requests.get(url_download_res_label, headers=headers)
+        res_data_and_label = json.loads(response.text)
+        res_data = res_data_and_label['res_data']
+        res_name = res_data_and_label['res_name']
+        res_label = res_data_and_label['res_label']
+        label = json.loads(res_label)
+        if len(label) == 1:
+            res_data = res_data[0]
+            img = base64.b64decode(res_data)
+            file = open(img_dir + '/' + res_name, 'wb')
+            file.write(img)
+            file.close()
+        else:  # res_name 需要改变一下
+            counter = 0
+            for sub_img in res_data:
+                img = base64.b64decode(sub_img)
+                res_name_list = res_name.split('.')
+                file = open(img_dir + '/' + res_name_list[0] + '_' + str(counter) + '.jpg', 'wb')
+                counter += 1
+                file.write(img)
+                file.close()
+
+        # get xml
+        xml_dir = path + '/' + 'xml'
+        if '.jpg' in res_name:
+            res_name = res_name.split('.jpg')[0]
+        elif '.png' in res_name:
+            res_name = res_name.split('.png')[0]
+        else:
+            res_name = res_name.split('.')[0]
+        if not os.path.isdir(xml_dir):
+            os.makedirs(xml_dir)
+
+        counter = 0
+        for name in label:
+            if name == '':
+                vocwriter = PascalVocWriter(foldername=xml_dir, filename=res_name, imgSize=(
+                    0, 0), databaseSrc='Unknown', localImgPath=xml_dir)
+                for i in label[name]:
+                    minx = int(i['vertexs'][0][0])
+                    miny = int(i['vertexs'][0][1])
+                    maxx = int(i['vertexs'][1][0])
+                    maxy = int(i['vertexs'][1][1])
+                    name = i['name']
+                    vocwriter.addBndBox(minx, miny, maxx, maxy, name, False)
+                xml_name = res_name + ".xml"
+                xml_path = os.path.join(xml_dir, xml_name)
+                vocwriter.save(targetFile=xml_path)
+            else:
+                vocwriter = PascalVocWriter(foldername=xml_dir, filename=res_name, imgSize=(
+                    0, 0), databaseSrc='Unknown', localImgPath=xml_dir)
+                for i in label[name]:
+                    minx = int(i['vertexs'][0][0])
+                    miny = int(i['vertexs'][0][1])
+                    maxx = int(i['vertexs'][1][0])
+                    maxy = int(i['vertexs'][1][1])
+                    name = i['name']
+                    vocwriter.addBndBox(minx, miny, maxx, maxy, name, False)
+                xml_name = res_name + '_' + str(counter) + ".xml"
+                counter += 1
+                xml_path = os.path.join(xml_dir, xml_name)
+                vocwriter.save(targetFile=xml_path)
+        index += 1
+        logging.info('the total number of res is ' + str(imgNumber) + ',' + 'now we are downloading res ' + str(index))  # 正常信息
+
+
+def login(email, password, url):  # 登陆模块
+    # url = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/auth/'
+    data = json.dumps({'email': email, 'password': password})
+    response = requests.post(url, data)  # 发出请求
+    res = json.loads(response.text)
+    if res != -1 and res != 0:
+        token = res['token']
+    else:
+        return {'error': '账号或密码错误'}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'jwt' + ' ' + token
+    }
+    return headers
+
+def download_train_res(project, version, path):  #项目 版本 本地保存路径
+    url = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/auth/'
+    data = json.dumps({'email': 'sys_admin', 'password': 'hongpu'})
+    response = requests.post(url, data)  # 发出请求
+    res = json.loads(response.text)
+    if res != -1 and res != 0:
+        token = res['token']
+    else:
+        return {'error': '账号或密码错误'}
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'jwt' + ' ' + token
+    }
+    url_prj = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/projects'
+    response = requests.get(url_prj, headers=headers)
+    prj_list = json.loads(response.text)
+    for i in prj_list:
+        if project==i['name']:
+            project_id = i['id']
+    url_version = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/label_versions/?pid=' + str(project_id)
+    response = requests.get(url_version, headers=headers)
+    version_list = json.loads(response.text)
+    for i in version_list:
+        if version==i['name']:
+            version_id = i['id']
+    url_get_resource_id_list = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/get_res_id_list/?label_version_id=' + str(
+        version_id)
+    response = requests.get(url_get_resource_id_list, headers=headers)
+    resource_id_list = json.loads(response.text)
+    threadWork(resource_id_list, version_id, headers, path)
+
+
+
+def testDownloadTrainRes():
+    # email = 'sys_admin'
+    # password = 'hongpu'
+    # url = 'http://vi-plat.chinaeast2.cloudapp.chinacloudapi.cn:8000/api/v1/auth/'
+    # headers = login(email, password, url)
+    project = '孙可标签测试'
+    version = 'v1'
+    path = r'C:\Users\86157\Desktop\testlin'
+    download_train_res(project, version, path)
+
+testDownloadTrainRes()
+
+
+
 
